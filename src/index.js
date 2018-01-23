4758d5e216c84f91aa57d7f2db3896c1bd79daa1
AaronCCWong <aarn.wong@gmail.com>
2018-01-23 16:52:50 -0500

    Move scss.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from 'components/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'index.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
