48dc8a5c95d61242ce051258456af5402bc4b3ea
AaronCCWong <aarn.wong@gmail.com>
2018-12-30 15:38:08 -0500

    Update bio and add meta tag

--- src/components/portfolio/Bio.jsx
+++ src/components/portfolio/Bio.jsx
@@ -10,10 +10,10 @@ const Bio = () => (
       </div>
 
       <p className="Portfolio-section--bio">
-        <b>Bio.</b> I am a software engineer currently working at Merrill Corporation.
-        I am also working towards my master's degree in computer science at NYU. I am
-        particularly interested in machine learning and its applications to computer vision
-        and natural language processing.
+        <b>Bio.</b> I am a software engineer currently working at Merrill Corporation. I am also a student at NYU working
+        towards a master's degree in computer science with a focus on intelligent systems. I am particularly
+        interested in machine learning and its applications to computer vision and natural language processing.
+        I also enjoy applying reinforcement learning algorithms to games.
       </p>
     </div>
   </section>
