81e46541bcff41ad14fdab4471d4eb89b5e94297
AaronCCWong <aarn.wong@gmail.com>
2019-05-18 19:22:23 -0400

    Upgrade webpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-snapshot';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App.jsx';
-import { unregister } from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 import 'font-awesome/css/font-awesome.min.css';
 
@@ -13,4 +13,4 @@ render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-unregister();
+serviceWorker.unregister();
