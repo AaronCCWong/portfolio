b6b1b6fdd775284272c590bf4f401f50baa87e56
AaronCCWong <aarn.wong@gmail.com>
2019-06-22 20:44:56 -0700

    Update bio

--- src/components/portfolio/Bio.jsx
+++ src/components/portfolio/Bio.jsx
@@ -10,7 +10,7 @@ const Bio = () => (
       </div>
 
       <p className="Portfolio-section--bio">
-        <b>Bio.</b> I am a software engineer currently working at Merrill Corporation. I am also a student at NYU working
+        <b>Bio.</b> I am also a student at NYU working
         towards a master's degree in computer science with a focus on intelligent systems. I am particularly
         interested in machine learning and its applications to computer vision and natural language processing.
         I also enjoy applying reinforcement learning algorithms to games.
